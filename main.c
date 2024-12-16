7c223b78557f00d19898806b5911e30cfcc64b82
Bingergie <i@hebing.me>
2024-12-16 16:32:09 +0800

    Added SDL_DestroyWindow and SDL_Quit statements

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 
-    const SDL_Window *window = SDL_CreateWindow("Hello World", 400, 400, 0);
+    SDL_Window *window = SDL_CreateWindow("Hello World", 400, 400, 0);
 
     if (window == NULL) {
         printf("SDL_CreateWindow failed: %s\n", SDL_GetError());
@@ -22,6 +22,8 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
